f871802bcfcf72c0222ac3a836f60ed768796ca2
KevinMallon <mallon.kevin@gmail.com>
2013-02-10 18:20:30 -0500

    fixed requests index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208182907) do
+ActiveRecord::Schema.define(:version => 20130210231529) do
 
   create_table "employees", :force => true do |t|
     t.string   "name"
